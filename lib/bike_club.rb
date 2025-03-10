01c36f04ad82fbb75a8de7f267aac617b67beaf4
Brady Blair <bblair321@yahoo.comm>
2025-03-10 10:46:14 -0700

    Finished

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -13,5 +13,7 @@ class BikeClub
   def biker_with_most_rides
     # max_by is looking at all the bikers. 
     @bikers.max_by { |biker| biker.rides.values.flatten.size } # For each biker we are counting how many rides they have
+    # we have biker representing each biker, we grab the rides has and grab all the values fomr it using .values
+    # I am using flatten to combine all of the indivdual arrays into one array of rides
   end
 end
